a1619b17eea26e2d99a0eedbbb5af2a223d79a07
Shmulik <big.levovsky@gmail.com>
2017-02-18 12:57:59 +0200

    Transpiling and Bundling chapters

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,6 +1,7 @@
 /**
  * Created by Rami L on 18/02/2017.
  */
-var chalk = require('chalk');
+//var chalk = require('chalk');
+import chalk from 'chalk';
 
 console.log( chalk.green('Starting app in dev mode...') )
---
+++ src/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Rami L on 18/02/2017.
+ */
+import './index.css';
+
+import numeral from 'numeral';
+
+const courseValue = numeral(1000).format('$0,0.00');
+debugger;
+console.log(`I would pay ${courseValue} for this awesome course!`);
